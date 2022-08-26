544e29b1e7ba5d5de5718f57245c5f1bfe40c7cf
deyvi.tziu <deyvi.tziu@LSTKTR242950>
2022-08-25 20:46:10 -0500

    Add project files.

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public  class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logguer = new ErrorLogger();
+            logguer.Log("a");
+            Assert.That(logguer.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullExcption(string error)
+        {
+            var logguer = new ErrorLogger();
+            //logguer.Log(error);
+            Assert.That(() => logguer.Log(error), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+            logger.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+        
+        
+    }
+}
